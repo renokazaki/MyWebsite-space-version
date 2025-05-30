d01f916a55823df8085f30a54446740250673a9f
ren <ren84690@gmail.com>
2025-05-30 23:52:31 +0900

    背景の星削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { Canvas } from "@react-three/fiber";
 import Experience from "./_components/Experience";
-import { ScrollControls, Scroll, Float } from "@react-three/drei";
+import { ScrollControls, Scroll } from "@react-three/drei";
 import { config } from "./config";
 import Interface from "./_components/Section/InterFace";
 import { MotionConfig } from "framer-motion";
 import Menu from "./_components/Section/Menu";
 import { Suspense } from "react";
 import { Loading } from "./_components/Loading";
-import {SpaceSky} from "./_components/ModelsComponents/SpaceSky";
 import { Earth } from "./_components/ModelsComponents/Earth";
 
 function App() {
@@ -15,10 +14,7 @@ function App() {
     <>
       <Loading />
       <Canvas camera={{ position: [0, 0.5, 5], fov: 42 }}>
-        <Float floatIntensity={1} speed={1}>
-          <SpaceSky />
-        </Float>
-        <Earth position={[7, -1, -40]} scale={6} rotation={[0, -1, 0]}/>
+        <Earth position={[7, -1, -40]} scale={6} rotation={[0, -1, 0]} />
         <ScrollControls
           pages={config.sections.length}
           damping={0.1}
